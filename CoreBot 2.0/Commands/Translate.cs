d43bc1b4debffe2a18038f9215290fd1d6b8be29
Passive <passivemodding@gmail.com>
2017-12-07 18:57:11 +1100

    Minor Additions and Fixes

--- CoreBot 2.0/Commands/Translate.cs
+++ CoreBot 2.0/Commands/Translate.cs
@@ -12,6 +12,33 @@ namespace CoreBot.Commands
 {
     public class Translate : ModuleBase
     {
+        private readonly CommandService _service;
+        public Translate(CommandService service)
+        {
+            _service = service;
+        }
+
+        [Command("help")]
+        [Summary("help")]
+        [Remarks("List of commands/info")]
+        public async Task Help()
+        {
+            var embed = new EmbedBuilder();
+            embed.Title = $"Translate Commands || `{Config.Load().Prefix} <command>`";
+            foreach (var module in _service.Modules)
+            {
+                var modulelist = "";
+                foreach (var command in module.Commands)
+                {
+                    modulelist += $"`{Config.Load().Prefix}{command.Summary}` - {command.Remarks}\n";
+                }
+                embed.AddField($"{module.Name}", $"{modulelist}");
+            }
+            embed.AddField("Designed By PassiveModding", $"{Config.Load().HomeInvite}\n" +
+                                                         $"https://passivenation.com/");
+            await ReplyAsync("", false, embed.Build());
+        }
+
         [Command("translate")]
         [Summary("translate <language-code> <message>")]
         [Remarks("Translate from one language to another")]
@@ -22,7 +49,7 @@ namespace CoreBot.Commands
             {
                 await ReplyAsync("Unsupported Language");
                 var embed2 = new EmbedBuilder();
-                embed2.AddField("Languages",
+                embed2.AddField("Language Codes",
                     "`fr` - french(français)\n" +
                     "`en` - english\n" +
                     "`es` - spanish(Español)\n" +
@@ -51,6 +78,21 @@ namespace CoreBot.Commands
 
             await ReplyAsync("", false, embed.Build());
             File.Delete(Path.Combine(AppContext.BaseDirectory, $"{Context.Message.Id}.txt"));
+        }
+
+        [Command("Languages")]
+        [Summary("Languages")]
+        [Remarks("Lists laguages supported by the bot")]
+        public async Task Languages()
+        {
+                var embed2 = new EmbedBuilder();
+                embed2.AddField("Languages",
+                    "`fr` - french(français)\n" +
+                    "`en` - english\n" +
+                    "`es` - spanish(Español)\n" +
+                    "`tl` - filipino\n" +
+                    "`pt` - portugese (português)");
+                await ReplyAsync("", false, embed2.Build());
         }
     }
 }
